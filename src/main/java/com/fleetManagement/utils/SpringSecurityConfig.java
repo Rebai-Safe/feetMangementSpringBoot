27dbe24975d63b89d3c8a414ee8e38b711323e64
safe rebai <saferebai@hotmail.com>
2020-05-20 11:11:19 +0200

    authentification in memory config

--- src/main/java/com/fleetManagement/utils/SpringSecurityConfig.java
+++ src/main/java/com/fleetManagement/utils/SpringSecurityConfig.java
@@ -1,18 +1,41 @@
 package com.fleetManagement.utils;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-
-
-/*
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity //tell spring security that this web security configuration to handle web request
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
-*/
-
-public class SpringSecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//set conf to auth object
+		
+		//tell which type of authentification desired ( chain method pattern)
+		auth.inMemoryAuthentication()
+		     .withUser("safe")
+		     .password("0000")
+		     .roles("USER");
+	}
+	
+	//pasword encoder actually does nothing here
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+		
+	}
+	
+	
+	
+	
+	
 
 	/*
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/").permitAll();
@@ -26,6 +49,6 @@ public class SpringSecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
 		.password("{noop}password") //no password encoder
 		.roles("USER");
 	}
-	
 	*/
+	
 }
